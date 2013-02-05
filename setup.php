4f83a27c3cf4d4f80baaf610422bf7fe938cb9ed
Charles Marshall <charles@oneblackbear.com>
2013-02-05 18:52:14 +0000

    need to include pathing info

--- setup.php
+++ setup.php
@@ -1,6 +1,11 @@
 <?
 CMSApplication::register_module("categories", array("display_name"=>"Categories", "link"=>"/admin/categories/"));
 
+AutoLoader::register_view_path("plugin", __DIR__."/view/");
+AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
+AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
+AutoLoader::$plugin_array[] = array("name"=>"wildfire.categories","dir"=>__DIR__);
+
 if(defined("CONTENT_MODEL")){
   WaxEvent::add(CONTENT_MODEL.".setup", function(){
     $model = WaxEvent::data();
