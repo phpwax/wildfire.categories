29be7d0db35ad2c498fdee45d54032e9522aa2f8
Charles Marshall <charles@oneblackbear.com>
2013-02-05 18:27:57 +0000

    admin controllers

---
+++ lib/controller/CMSAdminCategoryController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * admin section for categories - inherits methods from admin component
+ */
+
+class CMSAdminCategoryController extends AdminComponent {
+
+  public $module_name = "categories";
+  public $model_class = 'WildfireCategory';
+  public $display_name = "Site Categories";
+  public $dashboard = false;
+
+}
+
+?>
---
+++ resources/app/controller/admin/AdminCategoriesController.php
@@ -0,0 +1,2 @@
+<?php
+class AdminCategoriesController extends CMSAdminCategoryController {}
