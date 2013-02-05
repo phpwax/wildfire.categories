4e17c1141cdf1b9689b086c4083b953b43a9db0b
Charles Marshall <charles@oneblackbear.com>
2013-02-05 18:33:37 +0000

    adding in the join to the content model… maybe

--- setup.php
+++ setup.php
@@ -1,3 +1,14 @@
 <?
 CMSApplication::register_module("categories", array("display_name"=>"Categories", "link"=>"/admin/categories/"));
+
+if(!defined("CONTENT_MODEL")){
+  $con = new ApplicationController(false, false);
+  define("CONTENT_MODEL", $con->cms_content_class);
+}
+
+
+WaxEvent::add(CONTENT_MODEL.".setup", function(){
+  $model = WaxEvent::data();
+  $model->define("categories", "ManyToManyField", array('target_model'=>"WildfireCategory","eager_loading"=>true, "join_model_class"=>"WaxModelOrderedJoin", "join_order"=>"join_order", 'scaffold'=>false, 'group'=>'categories'));
+});
 ?>
