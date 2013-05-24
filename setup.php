a5f1d62de9bb94315dd3d88cee781825d544d9a5
mikenosko <michal@oneblackbear.com>
2013-05-24 16:27:32 +0100

    categories are in relationships in content edit

--- setup.php
+++ setup.php
@@ -9,7 +9,7 @@ AutoLoader::$plugin_array[] = array("name"=>"wildfire.categories","dir"=>__DIR__
 if(defined("CONTENT_MODEL")){
   WaxEvent::add(CONTENT_MODEL.".setup", function(){
     $model = WaxEvent::data();
-    $model->define("categories", "ManyToManyField", array('target_model'=>"WildfireCategory","eager_loading"=>true, "join_model_class"=>"WaxModelOrderedJoin", "join_order"=>"join_order", 'scaffold'=>false, 'group'=>'categories'));
+    $model->define("categories", "ManyToManyField", array('target_model'=>"WildfireCategory","eager_loading"=>true, "join_model_class"=>"WaxModelOrderedJoin", "join_order"=>"join_order", 'scaffold'=>false, 'group'=>'relationships'));
   });
 }
 ?>
